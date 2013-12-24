0feb9f852652a7a946d6b5fa6fddd3d3cf9b7ca4
axg <ajrosape@us.ibm.com>
2013-12-24 11:44:15 -0500

    Adding copyrights to UCD plugins and their dependencies Change-Id: Ib087787591792ec5eca2d02e2b08149d3ff7b3b3

--- src/main/groovy/com/urbancode/air/plugin/PropertiesHelper.groovy
+++ src/main/groovy/com/urbancode/air/plugin/PropertiesHelper.groovy
@@ -1,3 +1,14 @@
+/*
+* Licensed Materials - Property of IBM Corp.
+* IBM UrbanCode Build
+* IBM UrbanCode Deploy
+* IBM UrbanCode Release
+* IBM AnthillPro
+* (c) Copyright IBM Corporation 2002, 2013. All Rights Reserved.
+*
+* U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by
+* GSA ADP Schedule Contract with IBM Corp.
+*/
 package com.urbancode.air.plugin;
 
 public class PropertiesHelper {
