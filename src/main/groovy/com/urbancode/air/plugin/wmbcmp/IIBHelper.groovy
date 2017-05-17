7a23d771f47f758142901231305810b526119a88
Jeffrey Barto <jrbarto@us.ibm.com>
2017-05-17 10:26:18 -0400

    163362 Provide error output for non-existent execution group

--- src/main/groovy/com/urbancode/air/plugin/wmbcmp/IIBHelper.groovy
+++ src/main/groovy/com/urbancode/air/plugin/wmbcmp/IIBHelper.groovy
@@ -533,6 +533,11 @@ class IIBHelper {
     public void setExecutionGroup(String groupName) {
         if (groupName) {
             executionGroupProxy = brokerProxy.getExecutionGroupByName(groupName)
+
+            if (executionGroupProxy == null) {
+                throw new IllegalStateException("Execution group ${groupName} doesn't exist. Please make sure it's "
+                    + "name is spelled correctly and that it exists under the specified broker.")
+            }
         }
         else {
             throw new IllegalStateException("Execution group field specified with blank or null name.")
